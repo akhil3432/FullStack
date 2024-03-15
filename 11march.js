f21c0b17a29e2ad7717f2b7f458a97b71d7a804f
akhil3432 <akhilthakur89874@gmail.com>
2024-03-15 17:45:48 +0530

    made new repo

---
+++ 11march.js
@@ -0,0 +1,51 @@
+
+// let marks=90;
+
+// if(marks<90)
+// {
+//     console.log( "marks is less than 90");
+
+// }
+// else if(marks=90)
+// {
+//     console.log("marks is 90");
+
+// }
+// else{
+//     console.log("marks is not less than 90");
+// }
+// for(let i=1 ;i<9; i++)
+// {
+//     console.log("My value is :",i)
+// }
+// let i=1;
+// while(i<15)
+// {
+//   console.log(" my value is :",i);
+//   i=i+1;
+// }
+// let value=50;
+// do{ 
+//     console.log("Hello world");
+
+// }
+// while(value=50);
+// let a=20;
+// let b=30;
+// function addition(){
+//     let c=a+b;
+//     console.log(c);
+// }
+// let addition=(a,b)=>{
+//     let c=a+b;
+//      console.log(c);
+// }
+// addition(20,30);
+
+
+function output(){
+    console.log("js");
+    document.getElementById("hello").innerHTML="your responce is submited";
+}
+
+
